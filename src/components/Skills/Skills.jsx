e81d4b366780f1fd3d78c5d50adad3b315910c53
Rohit8146 <rohittandon545@gmail.com>
2025-09-24 20:50:25 +0530

    Bug Fixes

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,12 +1,11 @@
-import React from "react";
-import Image from "../Image/image";
+import Image from "../Image/Image";
 import "./skills.css";
 
 function Skills() {
   return (
     <div className="skills container">
       <h2>
-        I'm currently looking to join a 
+        I'm currently looking to join a
         <span className="purple-text"> cross-functional</span> team
       </h2>
       <p>that values improving people's lives through accessible design </p>
